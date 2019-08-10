adae0ff3c49edfce3a30388ae06273d3d95c05a7
Matthew Griffith <mdg.griffith@gmail.com>
2019-08-10 10:34:09 -0400

    button onPress can no longer be a maybe

--- src/Element/Input.elm
+++ src/Element/Input.elm
@@ -326,7 +326,7 @@ The `onPress` handler will be fired either `onClick` or when the element is focu
             , Element.focused
                 [ Background.color purple ]
             ]
-            { onPress = Just ClickMsg
+            { onPress = ButtonPressed
             , label = text "My Button"
             }
 
@@ -336,7 +336,7 @@ The `onPress` handler will be fired either `onClick` or when the element is focu
 button :
     List (Attribute msg)
     ->
-        { onPress : Maybe msg
+        { onPress : msg
         , label : Element msg
         }
     -> Element msg
@@ -363,15 +363,9 @@ button attrs { onPress, label } =
             :: focusDefault attrs
             :: Internal.Describe Internal.Button
             :: Internal.Attr (Html.Attributes.tabindex 0)
-            :: (case onPress of
-                    Nothing ->
-                        Internal.Attr (Html.Attributes.disabled True) :: attrs
-
-                    Just msg ->
-                        Events.onClick msg
-                            :: onEnter msg
-                            :: attrs
-               )
+            :: Events.onClick onPress
+            :: onEnter onPress
+            :: attrs
         )
         (Internal.Unkeyed [ label ])
 
