5ca3fcef30a7fb232ee6b0c756c20fd3e5d07065
Matthew Griffith <mdg.griffith@gmail.com>
2020-03-13 09:54:19 -0400

    use previous bash command

--- tests-rendering/automation/add-key-to-ssh.sh
+++ tests-rendering/automation/add-key-to-ssh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 eval $(ssh-agent -s) 
-ssh-add <(echo "$ELM_UI_TESTING_SSH_PRIVATE_KEY") 
+# ssh-add <(echo "$ELM_UI_TESTING_SSH_PRIVATE_KEY") 
+ssh-add - <<< "${ELM_UI_TESTING_SSH_PRIVATE_KEY}"
 
 # List out your new key's fingerprint
 ssh-add -l
