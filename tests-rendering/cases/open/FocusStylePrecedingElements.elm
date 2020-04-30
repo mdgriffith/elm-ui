338a0d506b64d3273b96f05e1e03b17b988909e3
Alex Korban <128337+alexkorban@users.noreply.github.com>
2020-05-01 08:24:38 +1200

    Add test case for issue 198

---
+++ tests-rendering/cases/open/FocusStylePrecedingElements.elm
@@ -0,0 +1,38 @@
+module FocusStylePrecedingElements exposing (main)
+
+{-|
+
+
+# `focused` style applied to an element when any element with a lower tab index is focused
+
+<https://github.com/mdgriffith/elm-ui/issues/198>
+
+When I add a focused attribute, the styles therein are applied not only when
+the element is focused, but also when any element with a lower tab index is focused.
+This behaviour is not browser-specific.
+
+To reproduce, tab through to the buttons in the Ellie below. As soon as the first
+button has focus, the third button (which is the only one with a focused attribute)
+is highlighted as well.
+
+I narrowed down the reason for this behaviour to this CSS selector:
+.s:focus ~ .fc-250-50-50-255-fs:not(.focus)
+
+Disabling it produces the correct focus behaviour.
+
+-}
+
+import Testable.Element exposing (..)
+import Testable.Element.Font as Font
+import Testable.Element.Input as Input
+
+
+main =
+    layout [] <|
+        column []
+            [ Input.button [] { onPress = Nothing, label = text "Button" }
+            , Input.button [] { onPress = Nothing, label = text "Button" }
+            , Input.button [ focused [ Font.color <| rgb255 250 50 50 ] ] { onPress = Nothing, label = text "Button" }
+            , Input.button [] { onPress = Nothing, label = text "Button" }
+            , Input.button [] { onPress = Nothing, label = text "Button" }
+            ]
