6d6035ea418b19c825e6358d8d03342e5194cf77
mdgriffith <mdgriffith@users.noreply.github.com>
2020-06-21 17:57:18 -0400

    css variable engine implementation

---
+++ src2/Internal/Flag2.elm
@@ -0,0 +1,325 @@
+module Internal.Flag2 exposing
+    ( Field(..)
+    , Flag(..)
+    , active
+    , add
+    , alignBottom
+    , alignRight
+    , behind
+    , bgColor
+    , bgGradient
+    , bgImage
+    , borderColor
+    , borderRound
+    , borderStyle
+    , borderWidth
+    , centerX
+    , centerY
+    , cursor
+    , flag
+    , focus
+    , fontAlignment
+    , fontColor
+    , fontFamily
+    , fontSize
+    , fontVariant
+    , fontWeight
+    , gridPosition
+    , gridTemplate
+    , height
+    , heightBetween
+    , heightContent
+    , heightFill
+    , heightTextAreaContent
+    , hover
+    , letterSpacing
+    , merge
+    , moveX
+    , moveY
+    , none
+    , overflow
+    , padding
+    , present
+    , rotate
+    , scale
+    , shadows
+    , spacing
+    , transparency
+    , txtShadows
+    , value
+    , width
+    , widthBetween
+    , widthContent
+    , widthFill
+    , wordSpacing
+    , xAlign
+    , yAlign
+    )
+
+{-| -}
+
+import Bitwise
+
+
+type Field
+    = Field Int Int
+
+
+type Flag
+    = Flag Int
+    | Second Int
+
+
+none : Field
+none =
+    Field 0 0
+
+
+value myFlag =
+    case myFlag of
+        Flag first ->
+            round (logBase 2 (toFloat first))
+
+        Second second ->
+            round (logBase 2 (toFloat second)) + 32
+
+
+{-| If the query is in the truth, return True
+-}
+present : Flag -> Field -> Bool
+present myFlag (Field fieldOne fieldTwo) =
+    case myFlag of
+        Flag first ->
+            Bitwise.and first fieldOne == first
+
+        Second second ->
+            Bitwise.and second fieldTwo == second
+
+
+{-| Add a flag to a field.
+-}
+add : Flag -> Field -> Field
+add myFlag (Field one two) =
+    case myFlag of
+        Flag first ->
+            Field (Bitwise.or first one) two
+
+        Second second ->
+            Field one (Bitwise.or second two)
+
+
+{-| Generally you want to use `add`, which keeps a distinction between Fields and Flags.
+
+Merging will combine two fields
+
+-}
+merge : Field -> Field -> Field
+merge (Field one two) (Field three four) =
+    Field (Bitwise.or one three) (Bitwise.or two four)
+
+
+flag : Int -> Flag
+flag i =
+    if i > 31 then
+        Second
+            (Bitwise.shiftLeftBy (i - 32) 1)
+
+    else
+        Flag
+            (Bitwise.shiftLeftBy i 1)
+
+
+
+{- Used for Style invalidation -}
+
+
+transparency =
+    flag 0
+
+
+padding =
+    flag 2
+
+
+spacing =
+    flag 3
+
+
+fontSize =
+    flag 4
+
+
+fontFamily =
+    flag 5
+
+
+width =
+    flag 6
+
+
+height =
+    flag 7
+
+
+bgColor =
+    flag 8
+
+
+bgImage =
+    flag 9
+
+
+bgGradient =
+    flag 10
+
+
+borderStyle =
+    flag 11
+
+
+fontAlignment =
+    flag 12
+
+
+fontWeight =
+    flag 13
+
+
+fontColor =
+    flag 14
+
+
+wordSpacing =
+    flag 15
+
+
+letterSpacing =
+    flag 16
+
+
+borderRound =
+    flag 17
+
+
+txtShadows =
+    flag 18
+
+
+shadows =
+    flag 19
+
+
+overflow =
+    flag 20
+
+
+cursor =
+    flag 21
+
+
+scale =
+    flag 23
+
+
+rotate =
+    flag 24
+
+
+moveX =
+    flag 25
+
+
+moveY =
+    flag 26
+
+
+borderWidth =
+    flag 27
+
+
+borderColor =
+    flag 28
+
+
+yAlign =
+    flag 29
+
+
+xAlign =
+    flag 30
+
+
+focus =
+    flag 31
+
+
+active =
+    flag 32
+
+
+hover =
+    flag 33
+
+
+gridTemplate =
+    flag 34
+
+
+gridPosition =
+    flag 35
+
+
+
+{- Notes -}
+
+
+heightContent =
+    flag 36
+
+
+heightFill =
+    flag 37
+
+
+widthContent =
+    flag 38
+
+
+widthFill =
+    flag 39
+
+
+alignRight =
+    flag 40
+
+
+alignBottom =
+    flag 41
+
+
+centerX =
+    flag 42
+
+
+centerY =
+    flag 43
+
+
+widthBetween =
+    flag 44
+
+
+heightBetween =
+    flag 45
+
+
+behind =
+    flag 46
+
+
+heightTextAreaContent =
+    flag 47
+
+
+fontVariant =
+    flag 48
---
+++ src2/Internal/Model2.elm
@@ -0,0 +1,436 @@
+module Internal.Model2 exposing (..)
+
+{-| The goal here is:
+
+1.  to be as thin of a wrapper as possible over `Html`
+2.  Use css variables instead of propagating up an entire stylesheet.
+
+-}
+
+import Html
+import Html.Attributes as Attr
+import Html.Keyed
+import Internal.Flag as Flag exposing (Flag)
+import Internal.StyleGenerator as Style
+import Json.Encode
+import VirtualDom
+
+
+type Element msg
+    = Element (Html.Html msg)
+    | Text String
+
+
+map : (a -> b) -> Element a -> Element b
+map fn el =
+    case el of
+        Text str ->
+            Text str
+
+        Element elem ->
+            Element (Html.map fn elem)
+
+
+mapAttr : (a -> b) -> Attribute a -> Attribute b
+mapAttr fn attr =
+    case attr of
+        NoAttribute ->
+            NoAttribute
+
+        Attr a ->
+            Attr (Attr.map fn a)
+
+        Link target url ->
+            Link target url
+
+        Download url filename ->
+            Download url filename
+
+        NodeName name ->
+            NodeName name
+
+        -- invalidation key and literal class
+        Class flag cls ->
+            Class flag cls
+
+        -- add to the style property
+        Style flag style ->
+            Style flag style
+
+        -- When using a css variable we want to attach the variable itself
+        -- and a class that implements the rule.
+        --               class  var       value
+        ClassAndStyle flag cls var ->
+            ClassAndStyle flag cls var
+
+        Nearby loc el ->
+            Nearby loc (map fn el)
+
+
+type Layout
+    = AsRow
+    | AsColumn
+    | AsEl
+    | AsGrid
+    | AsParagraph
+    | AsTextColumn
+    | AsRoot
+
+
+class cls =
+    Attr (Attr.class cls)
+
+
+type Attribute msg
+    = NoAttribute
+    | Attr (Html.Attribute msg)
+    | Link Bool String
+    | Download String String
+    | NodeName String
+      -- invalidation key and literal class
+    | Class Flag String
+      -- add to the style property
+    | Style Flag String
+      -- When using a css variable we want to attach the variable itself
+      -- and a class that implements the rule.
+      --               class  var       value
+    | ClassAndStyle Flag String String
+    | Nearby Location (Element msg)
+
+
+type Location
+    = Above
+    | Below
+    | OnRight
+    | OnLeft
+    | InFront
+    | Behind
+
+
+type Option
+    = FocusStyleOption FocusStyle
+    | RenderModeOption RenderMode
+
+
+type RenderMode
+    = Layout
+    | NoStaticStyleSheet
+    | WithVirtualCss
+
+
+{-| -}
+type alias FocusStyle =
+    { borderColor : Maybe Color
+    , backgroundColor : Maybe Color
+    , shadow :
+        Maybe
+            { color : Color
+            , offset : ( Int, Int )
+            , blur : Int
+            , size : Int
+            }
+    }
+
+
+focusDefaultStyle : FocusStyle
+focusDefaultStyle =
+    { backgroundColor = Nothing
+    , borderColor = Nothing
+    , shadow =
+        Just
+            { color =
+                Rgb 155 203 255
+            , offset = ( 0, 0 )
+            , blur = 0
+            , size = 3
+            }
+    }
+
+
+type NearbyChildren msg
+    = NoNearbyChildren
+    | ChildrenBehind (List (Html.Html msg))
+    | ChildrenInFront (List (Html.Html msg))
+    | ChildrenBehindAndInFront (List (Html.Html msg)) (List (Html.Html msg))
+
+
+element : Layout -> List (Attribute msg) -> List (Element msg) -> Element msg
+element layout attrs children =
+    render layout
+        children
+        "div"
+        Flag.none
+        ""
+        []
+        (contextClasses layout)
+        NoNearbyChildren
+        (List.reverse attrs)
+
+
+
+--  (List.reverse attrs)
+-- elementKeyed : Layout -> List (Attribute msg) -> List ( String, Element msg ) -> Element msg
+-- elementKeyed layout attrs children =
+--     let
+--         { name, htmlAttrs, nearby } =
+--             render "div" Flag.none "" [] (contextClasses layout) NoNearbyChildren (List.reverse attrs)
+--         renderedChildren =
+--             case nearby of
+--                 NoNearbyChildren ->
+--                     List.map (Tuple.mapSecond unwrap) children
+--                 ChildrenBehind behind ->
+--                     List.map (Tuple.pair "keyed") behind ++ List.map (Tuple.mapSecond unwrap) children
+--                 ChildrenInFront inFront ->
+--                     List.map (Tuple.mapSecond unwrap) children ++ List.map (Tuple.pair "keyed") inFront
+--                 ChildrenBehindAndInFront behind inFront ->
+--                     List.map (Tuple.pair "keyed") behind ++ List.map (Tuple.mapSecond unwrap) children ++ List.map (Tuple.pair "keyed") inFront
+--     in
+--     Element (Html.Keyed.node name htmlAttrs renderedChildren)
+
+
+unwrap : Element msg -> Html.Html msg
+unwrap el =
+    case el of
+        Text str ->
+            Html.span [ Attr.class textElementClasses ] [ Html.text str ]
+
+        Element html ->
+            html
+
+
+wrapText el =
+    case el of
+        Text str ->
+            Html.text str
+
+        Element html ->
+            html
+
+
+text =
+    Text
+
+
+
+-- text : String -> Element msg
+-- text str =
+--     Element (Html.span [ Attr.class textElementClasses ] [ Html.text str ])
+
+
+none : Element msg
+none =
+    Element (Html.text "")
+
+
+type alias Rendered msg =
+    { name : String
+    , htmlAttrs : List (VirtualDom.Attribute msg)
+    , nearby : NearbyChildren msg
+    , wrapped : List Wrapped
+    }
+
+
+type Wrapped
+    = InLink String
+
+
+render :
+    Layout
+    -> List (Element msg)
+    -> String
+    -> Flag.Field
+    -> String
+    -> List (VirtualDom.Attribute msg)
+    -> String
+    -> NearbyChildren msg
+    -> List (Attribute msg)
+    -> Element msg
+render layout children name has styles htmlAttrs classes nearby attrs =
+    case attrs of
+        [] ->
+            -- { name = name
+            -- , htmlAttrs =
+            -- Attr.class classes
+            --     :: Attr.property "style" (Json.Encode.string styles)
+            -- :: htmlAttrs
+            -- , nearby = nearby
+            -- , wrapped = []
+            -- }
+            let
+                renderedChildren =
+                    case nearby of
+                        NoNearbyChildren ->
+                            List.map unwrap children
+
+                        ChildrenBehind behind ->
+                            behind ++ List.map unwrap children
+
+                        ChildrenInFront inFront ->
+                            List.map unwrap children ++ inFront
+
+                        ChildrenBehindAndInFront behind inFront ->
+                            behind ++ List.map unwrap children ++ inFront
+            in
+            Element
+                (Html.node name
+                    (Attr.class classes
+                        :: Attr.property "style" (Json.Encode.string styles)
+                        :: htmlAttrs
+                    )
+                    renderedChildren
+                )
+
+        NoAttribute :: remain ->
+            render layout children name has styles htmlAttrs classes nearby remain
+
+        (Attr attr) :: remain ->
+            render layout children name has styles (attr :: htmlAttrs) classes nearby remain
+
+        (Link targetBlank url) :: remain ->
+            render layout
+                children
+                "a"
+                has
+                styles
+                (Attr.href url
+                    :: Attr.rel "noopener noreferrer"
+                    :: (if targetBlank then
+                            Attr.target "_blank"
+
+                        else
+                            Attr.target "_self"
+                       )
+                    :: htmlAttrs
+                )
+                classes
+                nearby
+                remain
+
+        (Download url downloadName) :: remain ->
+            render layout
+                children
+                "a"
+                has
+                styles
+                (Attr.href url
+                    :: Attr.download downloadName
+                    :: htmlAttrs
+                )
+                classes
+                nearby
+                remain
+
+        (NodeName nodeName) :: remain ->
+            render layout children nodeName has styles htmlAttrs classes nearby remain
+
+        (Class flag str) :: remain ->
+            if Flag.present flag has then
+                render layout children name has styles htmlAttrs classes nearby remain
+
+            else
+                render layout children name (Flag.add flag has) styles htmlAttrs (str ++ " " ++ classes) nearby remain
+
+        (Style flag str) :: remain ->
+            if Flag.present flag has then
+                render layout children name has styles htmlAttrs classes nearby remain
+
+            else
+                render layout children name (Flag.add flag has) (str ++ styles) htmlAttrs classes nearby remain
+
+        (ClassAndStyle flag cls sty) :: remain ->
+            if Flag.present flag has then
+                render layout children name has styles htmlAttrs classes nearby remain
+
+            else
+                render layout
+                    children
+                    name
+                    (Flag.add flag has)
+                    (sty ++ styles)
+                    htmlAttrs
+                    (cls ++ " " ++ classes)
+                    nearby
+                    remain
+
+        (Nearby loc elem) :: remain ->
+            render layout children name has styles htmlAttrs classes nearby remain
+
+
+textElementClasses : String
+textElementClasses =
+    Style.classes.any
+        ++ " "
+        ++ Style.classes.text
+        ++ " "
+        ++ Style.classes.widthContent
+        ++ " "
+        ++ Style.classes.heightContent
+
+
+rootClass =
+    String.join " "
+        [ Style.classes.root
+        , Style.classes.any
+        , Style.classes.single
+        ]
+
+
+rowClass =
+    Style.classes.any ++ " " ++ Style.classes.row
+
+
+columnClass =
+    Style.classes.any ++ " " ++ Style.classes.column
+
+
+singleClass =
+    Style.classes.any ++ " " ++ Style.classes.single
+
+
+gridClass =
+    Style.classes.any ++ " " ++ Style.classes.grid
+
+
+paragraphClass =
+    Style.classes.any ++ " " ++ Style.classes.paragraph
+
+
+pageClass =
+    Style.classes.any ++ " " ++ Style.classes.page
+
+
+contextClasses context =
+    case context of
+        AsRow ->
+            rowClass
+
+        AsColumn ->
+            columnClass
+
+        AsRoot ->
+            rootClass
+
+        AsEl ->
+            singleClass
+
+        AsGrid ->
+            gridClass
+
+        AsParagraph ->
+            paragraphClass
+
+        AsTextColumn ->
+            pageClass
+
+
+type Color
+    = Rgb Int Int Int
+
+
+formatColor : Color -> String
+formatColor (Rgb red green blue) =
+    "rgb("
+        ++ String.fromInt red
+        ++ ("," ++ String.fromInt green)
+        ++ ("," ++ String.fromInt blue)
+        ++ ")"
